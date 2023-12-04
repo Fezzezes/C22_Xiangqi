7e60656456658e9badf8e24f9d4f2e277423789a
Francois Bouchard <francois.bouchard.97@gmail.com>
2023-12-04 17:34:35 -0500

    Ajout de la methode estValide pour la sous-classe Bombarde + les testes JUNIT associés

--- src/xiangqi/Bombarde.java
+++ src/xiangqi/Bombarde.java
@@ -12,7 +12,9 @@ public class Bombarde extends Piece{
     //•	Pour l’étape 1, elle se déplace comme un char (rook)
     //•	Pour les étapes futures, lorsqu’il y aura d’autres pièces sur l’échiquier, elle peut capturer seulement une pièce s’il y a exactement une pièce entre la bombarde et sa cible.
 
-        return false;
+        //Le déplacement doit avoir la même ligne ou la même colonne
+        System.out.println("Même ligne ou colonne: "+(depart.getColonne() == arrivee.getColonne() || depart.getLigne() == arrivee.getLigne()));
+        return (depart.getColonne() == arrivee.getColonne() || depart.getLigne() == arrivee.getLigne());
     }
 
 }
